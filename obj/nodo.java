42619f867411a7b3921e3dd6b6b4dfcc54626b37
Facupancani <facupancani@hotmail.com>
2022-11-14 20:31:18 -0300

    Primer commit

---
+++ obj/nodo.java
@@ -0,0 +1,17 @@
+
+public class nodo {
+
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private int DNI;
+	private String intereses;
+	
+	public nodo(String Nombre, String Apellido, int Edad, int DNI, String Intereses){
+		this.nombre = Nombre;
+		this.apellido = Apellido;
+		this.edad = Edad;
+		this.DNI = DNI;
+		this.intereses = Intereses;
+	}
+}
